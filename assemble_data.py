1917873604bd8a0239e4c1f656934735278cdf68
Martin Rosellen <martin.rosellen.16@ucl.ac.uk>
2020-02-06 17:39:07 +0000

    added script to merge distance data with energies

---
+++ assemble_data.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+with open('./productions/mmpbsa_all_frames/frame_energies.csv','r') as f:
+    energies = []
+    flag = False
+    for line in f.readlines():
+        if 'DELTA' in line:
+            flag = True
+            continue
+        if flag:
+            line = line.split(',')
+            line = [ll.replace('\n','') for ll in line]
+            if len(line) > 1:
+                energies.append(line[-1])
+
+energies = np.asarray(energies)
+
+distances = pd.read_csv('./productions/binding_site_distance_all.dat', nrows=len(energies))
+distances['energy'] = energies
+
+print(distances)
+
