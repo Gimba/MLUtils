7631d3b780645db8a6f58bc5524c04555d1dc934
Martin Rosellen <martin.rosellen@fu-berlin.de>
2020-05-02 11:58:00 +0100

    refactoring and commenting

--- assemble_data.py
+++ assemble_data.py
@@ -3,28 +3,34 @@
 import numpy as np
 import pandas as pd
 
-with open('./productions/frame_energies.csv','r') as f:
+# read in MMGBSA Delta G values
+with open('./frame_energies_all.csv', 'r') as f:
     energies = []
+
+    # flag used to start reading in line data
     flag = False
     for line in f.readlines():
+        # Delta G values begin after the first occurence of DELTA
         if 'DELTA' in line:
             flag = True
+            # skip the next line since it is also a header
             continue
         if flag:
             line = line.split(',')
-            line = [ll.replace('\n','') for ll in line]
+            # since Delta G values are the last element of the line they also contain the new line character which
+            # gets removed here
+            line = [ll.replace('\n', '') for ll in line]
             if len(line) > 1:
                 energies.append(line[-1])
 
 energies = np.asarray(energies)
 
-distances = pd.read_csv('./productions/binding_site_distance_all.dat', nrows=len(energies), sep=r"\s+")
-distances['energy'] = energies
+# read in as many lines from the cpptraj generated file as there are values in energies
+metrics = pd.read_csv('binding_site_distance_all.dat', nrows=len(energies), header=0, delim_whitespace=True)
 
-column_titles = list(distances.columns)
-column_titles[0] = column_titles[-1]
-new_data = distances.reindex(columns=column_titles[:-1])
-new_data = new_data.sample(frac=1)
+# overwrite the first line of the cpptraj data frame with energy values
+metrics['#Frame'] = energies
+# rename first column to energies (not really necessary since column headers are not part of the training data file)
+metrics.rename(columns={'#Frame': 'energies'}, inplace=True)
 
-new_data.to_csv('./productions/training_data_distances_all.csv', index=None, header=None)
-
+metrics.to_csv('training_data.csv', sep=',', header=None, index=None)
